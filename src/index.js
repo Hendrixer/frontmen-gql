ab7383aac170e4f53c4fa4b4b95c42022fca3140
Scott Moss <willscottmoss@gmail.com>
2019-02-05 08:56:38 +0100

    chore

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import { createServer } from './server'
-
-createServer()
-  .then(server => {
-    return server.listen()
-  })
-  .then(({ url }) => console.log(`server ready on ${url}`))
-  .catch(e => console.error(e))
